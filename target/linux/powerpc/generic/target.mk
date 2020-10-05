40979f64a52b7e15ed10ca6a7c6fcb7ac4942307
Yvan Janssens <ik@yvanj.me>
2020-10-05 15:18:55 +0200

    Initial attempt at adding a powerpc target
    This is an initial attempt at adding a powerpc target, based off the existing x86 target

---
+++ target/linux/powerpc/generic/target.mk
@@ -0,0 +1,8 @@
+BOARDNAME:=Generic
+CPU_TYPE :=powerpc
+FEATURES += audio pci pcie usb
+
+define Target/Description
+	Build firmware images for the pmac32 target in QEMU.
+endef
+
---
+++ target/linux/powerpc/modules.mk
@@ -0,0 +1,6 @@
+#
+# Copyright (C) 2017 Cezary Jackiewicz <cezary@eko.one.pll>
+#
+# This is free software, licensed under the GNU General Public License v2.
+#
+
